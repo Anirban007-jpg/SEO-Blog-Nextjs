3c0bf17b628b1e81eea406f9a42479d14ad13f67
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-09 21:19:42 +0530

    Closes #2

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const morgan = require('morgan');
 const bp = require('body-parser');
 const cp = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config()
 
 
 // app
 const app = express();
 
+// db
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true}).then(() => console.log('DB connected'));
 
 // middlewares
 app.use(morgan('dev'))
@@ -16,7 +19,10 @@ app.use(bp.json())
 app.use(cp())
 
 //cors
-app.use(cors());
+if(process.env.NODE_ENV === 'development') {
+    app.use(cors({origin: `${process.env.CLIENT_URL}`}));    
+}
+
 
 // routes
 app.get('/api', (req,res)=> {
