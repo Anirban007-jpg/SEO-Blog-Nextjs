3e45bbd54dbb8f2c1ea8f1450814ffdd020d5661
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-09 23:08:07 +0530

    Closes #3

---
+++ backend/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        index: true,
+        lowerCase: true
+    },
+    name: {
+        type: String,
+    },
+    about: {
+        type: String
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+    profile: {
+        type: String,
+        required: true
+    },
+    hashed_password: {
+        type: String,
+        required: true
+    },
+    salt: String,
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    role: {
+        type: Number,
+        trim: true
+    },
+    resetPasswordLink: {
+        data: String,
+        default: ""
+    }
+}, {timestamp: true})
+
+module.exports = mongoose.model('User', userSchema);
