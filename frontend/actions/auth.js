5d35dddb5518e688ab1c6ce86adbfd66114e2d25
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-10 17:17:27 +0530

    Closes #15

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -1,5 +1,6 @@
 import fetch from 'isomorphic-fetch';
 import {API} from '../config';
+import cookie from 'js-cookie';
 
 export const signup = (user) => {
     return fetch(`${API}/signup`, {
@@ -29,4 +30,66 @@ export const signin = (user) => {
     .then(response => {
         return response.json();
     }).catch(err => console.log(err))
+}
+
+// set cookie
+export const setCookie = (key, value) => {
+    if(process.browser){
+        cookie.set(key, value, {
+            expires: 1
+        });
+    }
+};
+
+// remove cookie
+export const removeCookie = (key) => {
+    if(process.browser){
+        cookie.set(key, {
+            expires: 1
+        });
+    }
+};
+
+// get cookie
+export const getCookie = (key) => {
+    if(process.browser){
+        cookie.get(key);
+    }
+};
+
+// localStrorage
+export const setLocalStorage = (key,value) => {
+    if (process.browser){
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+}
+
+
+export const removeLocalStorage = (key,value) => {
+    if (process.browser){
+        localStorage.removeItem(key)
+    }
+}
+
+
+// authenticate user by pass data to cookie and localstorage
+export const authenticate = (data,next) => {
+    setCookie('token', data.token);
+    setLocalStorage('user', data.user);
+    next();
+}
+
+// this will give authentication user data
+
+export const isAuth = () => {
+    if (process.browser){
+        const cookieChecked = getCookie(token);
+        if (cookieChecked){
+            if (localStorage.getItem('user')){
+                return JSON.parse(localStorage.getItem('user'));
+            }else{
+                return false;
+            }
+        }
+    }
 }
--- frontend/components/auth/SigninComponent.js
+++ frontend/components/auth/SigninComponent.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { signin } from "../../actions/auth";
+import { authenticate, signin } from "../../actions/auth";
 import Router from 'next/router';
 
 const SigninComponent = () => {
@@ -31,7 +31,9 @@ const SigninComponent = () => {
                 // save user token to cookie
                 // save user info to localstorage
                 // authenticate user
-               Router.push('/');
+                authenticate(data, () => {
+                    Router.push('/');
+                })
             }
         })
          
