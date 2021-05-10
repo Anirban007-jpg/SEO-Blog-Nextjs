f1287b5fcbe789b91cff472c48fee3107df8af56
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-10 13:55:39 +0530

    Closes #11

---
+++ frontend/pages/signup.js
@@ -0,0 +1,12 @@
+//import Header from "./Header";
+import Layout from "../components/Layout";
+
+const Signup = () => {
+    return (
+        <Layout>
+            <h2>Signup page</h2>
+        </Layout>
+    );
+};
+
+export default Signup;
