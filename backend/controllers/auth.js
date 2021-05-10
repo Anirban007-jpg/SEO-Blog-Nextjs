8da681c73504c8dab352eb07b462bbcef2f0292a
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-10 11:28:29 +0530

    Closes #7

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,8 +1,31 @@
 const User = require('../models/user');
 const fo = require('formidable');
 const fs = require('fs');
+const sid = require('shortid');
 
 exports.signup = (req,res) => {
-  const {username,mobile_no,name,email,about,password,address} = req.body;
-  res.json({username,mobile_no,name,email,about,password,address});
+    User.findOne({email: req.body.email}).exec((err,user) => {
+      if (user){
+        return res.status(400).json({
+          error: "User already exsists"
+        })
+      }
+
+      const {name,email,about,address,mobile_no,password} = req.body;
+      let username = sid.generate();
+      let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+      let newUser = new User({name,email,about,address,mobile_no,password,username,profile});
+      newUser.save((err, success) => {
+        if (err){
+          return res.status(400).json({
+            error: err
+          })
+        }
+
+        res.json({
+          message: "Signup Success, Please Signin"
+        });
+      });
+    })
 }
--- backend/validators/auth.js
+++ backend/validators/auth.js
@@ -4,7 +4,6 @@ exports.userSignupValidator = [
     check('name').not().isEmpty().withMessage('Name is required'),
     check('email').isEmail().withMessage('Must be a valid email address'),
     check('password').isLength({min: 6}).withMessage('Password must contain atleast 6 charecters long').matches(/\d/).withMessage("Password must contain a number"),
-    check('username').not().isEmpty().withMessage('Username is mandatory').isLength({min:8, max:32}).withMessage('username must be between 6 to 32 charecters long'),
     check('about').not().isEmpty().withMessage('Mention something about the user'),
     check('mobile_no').not().isEmpty().withMessage('Mobile Number is mandatory').isLength({min:10 , max:11}).withMessage('Mobile No must be 11 charecters long'),
     check('address').not().isEmpty().withMessage('Address is mandatory.').isLength({max: 25}).withMessage('Address must be 30 charecters long'),
