ca8ddd1c0c88e4a346670c4d09316b629502c608
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-11 13:54:48 +0530

    Closes #24

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -2,17 +2,18 @@ const category = require('../models/category');
 const s = require('slugify');
 
 exports.create = (req,res) => {
+
+    const {name} = req.body;
+    let slug = s(name).toLowerCase();
     
-    category.findOne({name : req.body.name}).exec((err,cat) => {
+    category.findOne({slug : slug}).exec((err,cat) => {
         if (cat){
             return res.status(400).json({
                 error: "Category already exsits!"
             })
         }
     
-        let slug = s(name).toLowerCase();
-
-
+        
         let Category = new category({name, slug});
         
         Category.save((err, data) => {
@@ -24,5 +25,48 @@ exports.create = (req,res) => {
 
             res.json(data);
         })
+    })
+}
+
+exports.list = (req,res) => {
+    category.find({}).exec((err, data) => {
+        if (err) {
+                return res.status(400).json({
+                    error: err
+                })
+        }
+
+        res.json(data);
+    })
+}
+
+
+exports.read = (req,res) => {
+    const slug = req.params.slug.toLowerCase();
+
+    category.findOne({slug: slug}).exec((err,cat) => {
+        if (err || !cat) {
+            return res.status(400).json({
+                error: "Such Category does not exsists"
+            })
+        }
+
+        res.json(cat);
+    })
+}
+
+
+exports.Delete = (req,res) => {
+    const slug = req.params.slug.toLowerCase();
+    category.findOneAndRemove({slug: slug}).exec((err,cat) => {
+        if (err || !cat) {
+            return res.status(400).json({
+                error: "Such Category does not exsists and thus cannot be deleted!!"
+            })
+        }
+
+        res.json({
+            message: "Category deleted successfully"
+        });
     })
 }
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { requireSignin, authMiddleware, adminMiddleware, superadminMiddleware } = require('../controllers/auth');
-const { create } = require('../controllers/category');
+const { create,read,list,Delete } = require('../controllers/category');
 const { runValidation } = require('../validators');
 const { CategoryCreateValidator } = require('../validators/category');
 
 
 router.post('/category/create', CategoryCreateValidator, runValidation, requireSignin, adminMiddleware, create);
+router.get('/categories', list);
+router.get('/category/:slug', read);
+// router.put('/category/:slug', requireSignin, adminMiddleware, update);
+router.delete('/category/:slug', requireSignin, adminMiddleware, Delete);
 
 
 module.exports = router;
