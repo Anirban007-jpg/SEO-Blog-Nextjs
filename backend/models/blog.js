2dc4f9d8415d5efdd0a7386dc32d7d2f6c868efa
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-11 20:42:52 +0530

    Closes #33

---
+++ backend/models/blog.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const {ObjectId} = mongoose.Schema;
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        unique: true,
+        index: true,
+    },
+    slug: {
+        type: String,
+        unique: true,
+        index: true
+    },
+    body: {
+        type: {}
+    },
+    excerpt: {
+        type: String
+    },
+    mtitle: {
+        type: String,
+    },
+    mdescription: {
+        type: String,
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    categories: [{type:ObjectId, ref: 'Category', required: true}],
+    tags: [{type:ObjectId, ref: 'Tag', required: true}],
+    postedBy : {
+        type:ObjectId, 
+        ref: 'User', 
+        required: true
+    }
+}, {timestamp: true})
+
+
+module.exports = mongoose.model('Blog', blogSchema);
