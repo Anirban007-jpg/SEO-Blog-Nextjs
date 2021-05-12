aee6bff73944bf85f6cb9f3d5b4754aebd620144
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-12 10:26:37 +0530

    Closes #38

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -1,10 +1,23 @@
+import Link from "next/link";
+import  Router from "next/router";
+import { useState, useEffect } from "react";
+import dynamic from 'next/dynamic';
+import { withRouter } from "next/router";
+import { isAuth, getCookie } from "../../actions/auth";
+import {list} from '../../actions/category';
+import {listTags} from '../../actions/tag';
+import { createBlog } from "../../actions/blog";
+const Quill = dynamic(() => import('react-quill'), {ssr: true})
+import '../../node_modules/react-quill/dist/quill.snow.css';
 
-const BlogCreate = () => {
+
+const BlogCreate = ({router}) => {
     return (
         <div>
-            <h2>Create Blog form</h2>            
+            <h2>Create Blog form</h2>
+            {/* {JSON.stringify(router.route)}             */}
         </div>
     )
 }
 
-export default BlogCreate
+export default withRouter(BlogCreate)
