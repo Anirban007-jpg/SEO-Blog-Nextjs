9949f8ac0d56b798d09bc20d66fbfe0e73d7c397
Anirban Banerjee <abanerjee763@outlook.com>
2021-06-06 13:21:43 +0530

    Closes #93

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {signup, signin, signout,forgotPassword, resetPassword,  preSignup } = require('../controllers/auth');
+const {signup, signin, signout,forgotPassword, resetPassword,  preSignup, googleLogin} = require('../controllers/auth');
 const { runValidation } = require('../validators');
 const { userSignupValidator, userSigninValidator,     forgotPasswordValidator,
     resetPasswordValidator } = require('../validators/auth');
@@ -12,5 +12,9 @@ router.post('/signin', userSigninValidator, runValidation, signin);
 router.post('/signout', signout);
 router.put('/forgot-password', forgotPasswordValidator, runValidation, forgotPassword);
 router.put('/reset-password', resetPasswordValidator, runValidation, resetPassword);
+
+
+// google login
+router.post('/google-login', googleLogin);
 
 module.exports = router;
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ module.exports = withCSS({
         API_DEVELOPMENT: 'http://localhost:8000/api',
         PRODUCTION: false,
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-        DISQUS_SHORTNAME: 'seoblog-ucno9dvq83'
-
+        DISQUS_SHORTNAME: 'seoblog-ucno9dvq83',
+        GOOGLE_CLIENT_ID: '76720414364-e8auk97b1h9q8d9o5ck58ohiug61ddj1.apps.googleusercontent.com'
     }
 })
